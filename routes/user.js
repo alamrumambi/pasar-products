84a57de6c10ced9ca94c8a5d47f2e1ad7486b78c
alamrumambi <alam.abumaryam@gmail.com>
2021-11-13 01:29:56 +0700

    fixing auth in users route

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
 const { register, login, getLogin, logout, getRegister, findAll, getUpdate, postUpdate, deleteUser, search } = require('../controllers/userCotroller');
+const { authentication } = require('../middlewares/auth');
 
+router.get('/login', getLogin);
+router.post('/login', login);
+router.use(authentication);
 router.get('/', findAll);
 router.get('/register', getRegister);
 router.post('/register', register);
 router.get('/edit/:id', getUpdate);
 router.post('/edit/:id', postUpdate);
-router.get('/login', getLogin);
-router.post('/login', login);
 router.get('/delete/:id', deleteUser);
-router.get('/logout', logout);
 router.post('/search', search);
+router.get('/logout', logout);
 
 module.exports = router;
